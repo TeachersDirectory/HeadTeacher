bce997b48593bd91085986f227a86d49225b71fc
TeachersDirectory <83336056+TeachersDirectory@users.noreply.github.com>
2026-06-17 12:58:58 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -81,9 +81,23 @@ function renderTeachers(list = teachers) {
             <div class="action-area">
 
                 <button
-                    class="edit-btn"
-                    onclick="goToEdit(${teacher.id})">
-                    ✏️
+                   class="edit-btn"
+                   onclick="goToEdit(${teacher.id})">
+
+                       <svg viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            width="20"
+                            height="20">
+
+                           <path d="M12 20h9"/>
+                           <path d="M16.5 3.5a2.1 2.1 0 0 1 3 3L7 19l-4 1 1-4 12.5-12.5z"/>
+
+                       </svg>
+
                 </button>
 
                 <a
