346cc61a6c3f92fc980b9bc565bc03500a8600a3
TeachersDirectory <83336056+TeachersDirectory@users.noreply.github.com>
2026-06-18 08:14:31 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -68,8 +68,8 @@ function renderTeachers(list = teachers) {
                             stroke-width="2"
                             stroke-linecap="round"
                             stroke-linejoin="round"
-                            width="18"
-                            height="18">
+                            width="16"
+                            height="16">
 
                             <path d="M12 20h9"/>
                             <path d="M16.5 3.5a2.1 2.1 0 0 1 3 3L7 19l-4 1 1-4 12.5-12.5z"/>
